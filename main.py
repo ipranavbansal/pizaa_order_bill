a24e12c98c67c8bea90b8c14ba72e4f617e13aaa
Pranav Bansal <40292040+ipranavbansal@users.noreply.github.com>
2023-02-05 21:09:28 +0000

    upgraded code

--- main.py
+++ main.py
@@ -3,38 +3,22 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
-    if add_pepperoni=="Y":
+bill  = 0
 
-        if size=="S":
-            bill+=15 
-            bill+=2
-            print("Your final bill is: $" + str(bill) )
-
-        if size=="M":
-            bill+=20
-            bill+=3
-            print("Your final bill is: $" + str(bill) )
-
-        if size=="L":
-            bill+=25
-            bil+=3 
-            print("Your final bill is: $" + str(bill) )
-
-    elif add_pepperoni == "N":
-        if size=="S":
-            bill=15 
-            print("Your final bill is: $" + str(bill) )
-
-        if size == "M":
-            bill=20
-            print("Your final bill is: $" + str(bill) )
-
-        if  size == "L":
-            bill=25 
-            print("Your final bill is: $" + str(bill) )
-
-
-if extra_cheese == "Y":
-    bill=1
+if size == "S":
+  bill += 15
+elif size == "M":
+  bill += 20
+else:
+  bill += 25
 
 if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
+    
+if extra_cheese == "Y":
+  bill += 1
+  
+print(f"Your final bill is: ${bill}.")
