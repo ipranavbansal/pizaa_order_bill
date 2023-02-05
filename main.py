e6f1218cb3df3b61be207e3b0684c8bc55f56f3a
Pranav Bansal <40292040+ipranavbansal@users.noreply.github.com>
2023-02-06 02:18:06 +0530

    Pizza order bill value program using multiple Ifs

---
+++ main.py
@@ -0,0 +1,74 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill=0
+if extra_cheese =="N":
+# bill=0
+
+    if add_pepperoni=="Y":
+
+        if size=="S":
+            bill+=15 
+            bill+=2
+            print("Your final bill is: $" + str(bill) )
+
+        if size=="M":
+            bill+=20
+            bill+=3
+            print("Your final bill is: $" + str(bill) )
+
+        if size=="L":
+            bill+=25
+            bil+=3 
+            print("Your final bill is: $" + str(bill) )
+
+    elif add_pepperoni == "N":
+        if size=="S":
+            bill=15 
+            print("Your final bill is: $" + str(bill) )
+
+        if size == "M":
+            bill=20
+            print("Your final bill is: $" + str(bill) )
+
+        if  size == "L":
+            bill=25 
+            print("Your final bill is: $" + str(bill) )
+
+
+if extra_cheese == "Y":
+    bill=1
+
+    if add_pepperoni == "Y":
+
+        if size == "S":
+            bill+=15 
+            print("Your final bill is: $" + str(bill) )
+
+        if size == "M":
+            bill+=20
+            bill+=3
+            print("Your final bill is: $" + str(bill) )
+        if size == "L":
+            bill+=25
+            bil+=3 
+            print("Your final bill is: $" + str(bill) )
+
+    elif add_pepperoni == "N":
+        if size=="S":
+            bill+=15 
+            print("Your final bill is: $" + str(bill) )
+
+        if size == "M":
+            bill+=20
+            print("Your finnal bill is: " + str(bill) )
+
+        if size == "L":
+            bill+=25 
+            print("Your final bill is: $" + str(bill) )
+
+
+    
+            
+            
